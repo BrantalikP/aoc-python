bcae78663e2ac35b19dedbc1c902e8feebc46243
Petr Brantalík <grounipteam@gmail.com>
2024-12-05 20:21:57 +0100

    day4 pt2

--- day4/pt2/pt2.py
+++ day4/pt2/pt2.py
@@ -1,6 +1,48 @@
 
+
+
+def lookDiagonal(array, rev: bool):
+	total = list()
+
+	for i in range(len(array) - 2):
+		for k in range(len(array[0]) - 2):
+			first = array[i][k]
+			second = array[i + 1][k + 1]
+			third = array[i + 2][k + 2]
+			if first + second + third == 'MAS' or first + second + third == 'SAM':
+				if rev:
+					total.append(f"{(i + 1)}:{len(array[0]) - 1 - (k + 1)}")
+				else:
+					total.append(f"{i + 1}:{k + 1}")
+	return total
+
+
+
+def createMap(ar: list[str]):
+	map = []
+	for index, i in enumerate(ar):
+		map.append(list(i))
+	return map
+
+
+
 def main(file_name):
-	return 0
+	file = read_file(file_name)
+	map = createMap(list(file))
+
+	diagonal = lookDiagonal(map, False)
+	diagonal_ver = lookDiagonal([list(reversed(sublist)) for sublist in map], True)
+	set1 = set(diagonal)
+	set2 = set(diagonal_ver)
+
+	matches = set1 & set2
+
+	return len(matches)
+
+
+def read_file(file_name):
+	with open(file_name, 'r') as f:
+		return f.read().splitlines()
 
 
 if __name__ == '__main__':
